bf72bead145d3d52583213676599a7e69a68c643
Glaucia Isidoro <glaucia.isidoro@metasix.com.br>
2017-08-15 11:29:06 -0300

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'produtos#index'
-  get "/produtos/new" => "produtos#new"
-  post "/produtos" => "produtos#create"
-  delete "/produtos/:id" => "produtos#destroy", as: :produto
+  resources :produtos, only: [:new, :create, :destroy]
 
 end
