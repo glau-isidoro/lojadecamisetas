44deddfb0b04736549bd7e735f89456530f9c447
Glaucia Isidoro <glaucia.isidoro@metasix.com.br>
2017-08-15 11:24:01 -0300

    adicionando e removendo produtos da tabela

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -5,4 +5,19 @@ class ProdutosController < ApplicationController
     @produtos_por_preco = Produto.order(:preco).limit 2
   end
 
+  def new
+  end
+
+  def create
+    valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
+    produto = Produto.create valores
+    redirect_to root_url
+  end
+
+  def destroy
+    id = params[:id]
+    Produto.destroy id
+    redirect_to root_url
+  end
+
 end
