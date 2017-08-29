f656714162a2ea348b57714220cd3b97d1b02ba1
Glaucia Isidoro <glaucia.isidoro@metasix.com.br>
2017-08-29 18:53:59 -0300

    refactor produtos controller - partial form new e edit

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,4 +1,5 @@
 class ProdutosController < ApplicationController
+  before_action :set_produto, only: [:edit, :update, :destroy]
 
   def index
     @produtos_por_nome = Produto.order(:nome).limit 5
@@ -11,46 +12,52 @@ class ProdutosController < ApplicationController
   end
 
   def create
-    valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
-    @produto = Produto.new valores
+    @produto = Produto.new produto_params
     if @produto.save
       flash[:notice] = "Produto salvo com sucesso!"
       redirect_to root_url
     else
-      @departamentos = Departamento.all
-      render :new
+      renderiza :new
     end
   end
 
   def edit
-    id = params[:id]
-    @produto = Produto.find id
-    @departamentos = Departamento.all
-    render :new
+    renderiza :edit
   end
 
   def update
-    id = params[:id]
-    @produto = Produto.find id
-    valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
-    if @produto.update valores
+    if @produto.update produto_params
       flash[:notice] = "Produto atualizado com sucesso!"
       redirect_to root_url
     else
-      @departamentos = Departamento.all
-      render :new
+      renderiza :edit
     end
   end
 
   def destroy
-    id = params[:id]
-    Produto.destroy id
+    @produto.destroy
     redirect_to root_url
   end
 
   def busca
     @nome_busca = params[:nome]
     @produtos = Produto.where "nome like ?", "%#{@nome_busca}%"
+  end
+
+  private
+
+  def renderiza(view)
+    @departamentos = Departamento.all
+    render view
+  end
+
+  def set_produto
+    id = params[:id]
+    @produto = Produto.find id
+  end
+
+  def produto_params
+    params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
   end
 
 end
